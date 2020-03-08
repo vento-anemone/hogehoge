23ae4a27857bf0d717228453d114e0032c4dad97
vento-anemone <33600962+vento-anemone@users.noreply.github.com>
2020-03-08 23:36:47 +0900

    String lastIndex

--- hoge/hoge/AppDelegate.swift
+++ hoge/hoge/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let str = "aaa,bbb,ccc,ddd"
+        var sss = str
+        if 16 < sss.count {
+            let index = str.lastIndex(of: ",")?.encodedOffset ?? 0
+            sss = String(str.prefix(index + 1))
+        }
+        
+        NSLog("sss \(sss)")
+        
         return true
     }
 
